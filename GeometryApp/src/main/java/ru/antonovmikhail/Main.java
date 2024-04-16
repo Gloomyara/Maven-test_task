cb429ab5ff3a6b9ee4ca95d5a0f9a641972035dd
Gloomyara <gloomyara@gmail.com>
2024-04-16 12:44:49 +0400

    Geometry utils v 1.1-SNAPSHOT

--- GeometryApp/src/main/java/ru/antonovmikhail/Main.java
+++ GeometryApp/src/main/java/ru/antonovmikhail/Main.java
@@ -5,6 +5,9 @@ import ru.antonovmikhail.shapes.Rectangle;
 import ru.antonovmikhail.shapes.Shape;
 import ru.antonovmikhail.shapes.Triangle;
 
+import static ru.antonovmikhail.ShapeManager.compareShapeArea;
+import static ru.antonovmikhail.ShapeManager.getRandomShape;
+
 public class Main {
     public static void main(String[] args) {
         Shape circle = new Circle(5);
@@ -16,6 +19,22 @@ public class Main {
                 rectangle.getShapePerimeter(), rectangle.getShapeArea());
         System.out.printf("Периметр 2д треугольника: %d;\n Площадь: %d; \n",
                 triangle.getShapePerimeter(), triangle.getShapeArea());
+        System.out.println("Площадь a больше площади б?:" + compareShapeArea(circle, rectangle));
+        for (int i = 0; i < 5; i++) {
+            Shape shape = getRandomShape();
+            if (shape instanceof Circle) {
+                System.out.printf("Длина 2д окружности: %d;\n Площадь: %d; \n",
+                        shape.getShapePerimeter(), shape.getShapeArea());
+            } else if (shape instanceof Triangle) {
+                System.out.printf("Периметр 2д треугольника: %d;\n Площадь: %d; \n",
+                        shape.getShapePerimeter(), shape.getShapeArea());
+            } else if (shape instanceof Rectangle) {
+                System.out.printf("Периметр 2д прямоугольника: %d;\n Площадь: %d; \n",
+                        shape.getShapePerimeter(), shape.getShapeArea());
+            } else {
+                System.out.println("Что-то пошло не так");
+            }
+        }
     }
 
 }
--- GeometryLibrary/GeometryUtils/src/main/java/ru/antonovmikhail/ShapeManager.java
+++ GeometryLibrary/GeometryUtils/src/main/java/ru/antonovmikhail/ShapeManager.java
@@ -1,15 +1,31 @@
 package ru.antonovmikhail;
 
 
+import ru.antonovmikhail.shapes.Circle;
+import ru.antonovmikhail.shapes.Rectangle;
 import ru.antonovmikhail.shapes.Shape;
+import ru.antonovmikhail.shapes.Triangle;
+
+import java.util.Random;
 
 public class ShapeManager {
 
-    public static int compareShapeArea(Shape s1, Shape s2) {
-        return s1.getShapeArea() - s2.getShapeArea();
+    public static boolean compareShapeArea(Shape s1, Shape s2) {
+        return s1.getShapeArea() > s2.getShapeArea();
     }
 
-    public static int compareShapePerimeter(Shape s1, Shape s2) {
-        return s1.getShapePerimeter() - s2.getShapePerimeter();
+    public static boolean compareShapePerimeter(Shape s1, Shape s2) {
+        return s1.getShapePerimeter() > s2.getShapePerimeter();
+    }
+
+    public static Shape getRandomShape() {
+        Random random = new Random();
+        int i = random.nextInt(3);
+        return switch (i) {
+            case 0 -> new Circle(random.nextInt(1, 25));
+            case 1 -> new Rectangle(random.nextInt(1, 25), random.nextInt(1, 25));
+            case 2 -> new Triangle(random.nextInt(1, 25), random.nextInt(1, 25));
+            default -> null;
+        };
     }
 }
